3408de1d5871fca185766aeea1a73ba06e13ad96
Dark-dev64 <jlescanosempertegui@gmail.com>
2026-07-19 08:00:53 -0500

    Ultimos cambios subidos

--- backend/app/api/pagos/route.js
+++ backend/app/api/pagos/route.js
@@ -17,6 +17,8 @@ export async function GET(request) {
 }
 
 export async function POST(request) {
+  const evento = await request.json();
+  console.log("🔔 Webhook Culqi recibido:", JSON.stringify(evento, null, 2));
   const body = await request.json();
 
   const { data, error } = await supabaseAdmin
