b0c353388e88c1cb5829f5267b17154db1d974dd
Dark-dev64 <jlescanosempertegui@gmail.com>
2026-07-20 03:44:33 -0500

    Integración con mercado pago

--- whatsapp-service/index.js
+++ whatsapp-service/index.js
@@ -14,6 +14,7 @@ const client = new Client({
   authStrategy: new LocalAuth({ clientId: "sistema-cip" }),
   puppeteer: {
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
 });
